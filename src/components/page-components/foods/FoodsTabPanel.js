761ca2073eb738d590136e7c5ac01d15323ccb79
Ikeji Benjamin <5072922+benmalcom@users.noreply.github.com>
2022-08-15 09:35:26 +0100

    Handle error

--- src/components/page-components/foods/FoodsTabPanel.js
+++ src/components/page-components/foods/FoodsTabPanel.js
@@ -1,33 +1,43 @@
-import { Flex, Spinner } from '@chakra-ui/react';
-import { useEffect, useMemo, useState } from 'react';
+import { Button, Flex, Spinner, Text } from '@chakra-ui/react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import FoodList from 'components/page-components/foods/FoodList';
 import FoodsFilter from 'components/page-components/foods/FoodsFilter';
 import { getFoods } from 'services/food';
 
 export const FoodsTabPanel = () => {
   const [currentFilter, setCurrentFilter] = useState(null);
-  const [foods, setFoods] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [foods, setFoods] = useState(undefined);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
+  const fetchFoods = useCallback(() => {
     setIsLoading(true);
     getFoods()
       .then(response => {
         setFoods(response.data);
       })
-      .catch(err => setFoods([]))
+      .catch(err => {
+        setFoods([]);
+        setError(true);
+      })
       .finally(() => setIsLoading(false));
-
-    return () => {};
   }, []);
 
+  useEffect(() => {
+    fetchFoods();
+    return () => {
+      setFoods(undefined);
+      setError(false);
+    };
+  }, [fetchFoods]);
+
   const filteredFoods = useMemo(() => {
-    return foods.filter(({ name }) =>
+    return foods?.filter(({ name }) =>
       name.toUpperCase().includes(currentFilter?.text?.trim().toUpperCase())
     );
   }, [foods, currentFilter?.text]);
 
-  if (isLoading)
+  if (isLoading || !foods)
     return (
       <Flex align="center" justify="center" w="full" h="full" mt={20}>
         <Spinner
@@ -39,6 +49,23 @@ export const FoodsTabPanel = () => {
         />
       </Flex>
     );
+
+  if (!isLoading && error)
+    return (
+      <Flex
+        flexDirection="column"
+        align="center"
+        justify="center"
+        w="full"
+        mt={30}
+      >
+        <Text>An Error occurred while fetching your food catalogue.</Text>
+        <Button variant="primary" mt={5} onClick={fetchFoods}>
+          Try again
+        </Button>
+      </Flex>
+    );
+
   return (
     <>
       <FoodsFilter
